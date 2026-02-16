738bcca261f66d846dc1e9a2c6898602a4cc8cf2
GUGOFO <gugofogomes@gmail.com>
2026-02-16 18:47:07 -0300

    aleatorizando array

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 const reiniciar = document.getElementById("reiniciar")
 let nomes = ["", "", ""], habilidades = ["", "", ""], tipos = ["", "", ""], imagens = ["", "", ""];
+let nomeResposta, habilidadeResposta, tipoResposta, imagemResposta;
 let ids = [1,2,3]
 
 reiniciar.addEventListener("click",async () => {
     for(let pokemon = 0; pokemon < 3; pokemon++){
         let id = Math.floor(Math.random() * 1000) + 1;
-        console.log(id);
         let dados = await dadosPokemon(id);
-        
-        ({ name: nomes[pokemon],
-          abilities: habilidades[pokemon],
-          sprites: { other: { "official-artwork": { front_default: imagens[pokemon] } } },
-          types : tipos[pokemon]
-        } = dados);
 
-        console.log(nomes[pokemon])
-        console.log(habilidades[pokemon])
-        console.log(imagens[pokemon])
-        console.log(tipos[pokemon])
+        ({ name: nomes[pokemon],
+            abilities: { 0: { ability: { name: habilidades[pokemon]}}},
+            sprites: { other: { "official-artwork": { front_default: imagens[pokemon] } } },
+            types : { 0: { type: { name: tipos[pokemon] } } }
+        } = dados);
     }
+
+    let indexPokemonResposta = Math.floor(Math.random() * 3);
+    nomeResposta = nomes[indexPokemonResposta];
+    habilidadeResposta = habilidades[indexPokemonResposta];
+    tipoResposta = tipos[indexPokemonResposta];
+    imagemResposta = imagens[indexPokemonResposta];
+
+    nomes.sort(() => Math.random() - 0.5);
+    habilidades.sort(() => Math.random() - 0.5);
+    tipoResposta.sort(() => Math.random() - 0.5);
+
+    console.log(`nome: ${nomeResposta}\n habilidade: ${habilidadeResposta}\n tipo: ${tipoResposta}`)
 })
 
 async function dadosPokemon(id) {
