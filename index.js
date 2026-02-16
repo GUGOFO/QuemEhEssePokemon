4f18cbce55ec389c2c96c508dbc39b8332e92c1a
GUGOFO <gugofogomes@gmail.com>
2026-02-16 18:20:22 -0300

    refatorando o JS

--- index.js
+++ index.js
@@ -1,42 +1,34 @@
-const reiniciar = getElementById("reiniciar")
-let nomeReal, habilidadeReal, tipoReal;
+const reiniciar = document.getElementById("reiniciar")
+let nomes = ["", "", ""], habilidades = ["", "", ""], tipos = ["", "", ""], imagens = ["", "", ""];
+let ids = [1,2,3]
 
-dadosPokemon()
+reiniciar.addEventListener("click",async () => {
+    for(let pokemon = 0; pokemon < 3; pokemon++){
+        let id = Math.floor(Math.random() * 1000) + 1;
+        console.log(id);
+        let dados = await dadosPokemon(id);
+        
+        ({ name: nomes[pokemon],
+          abilities: habilidades[pokemon],
+          sprites: { other: { "official-artwork": { front_default: imagens[pokemon] } } },
+          types : tipos[pokemon]
+        } = dados);
 
-
-
+        console.log(nomes[pokemon])
+        console.log(habilidades[pokemon])
+        console.log(imagens[pokemon])
+        console.log(tipos[pokemon])
+    }
+})
 
 async function dadosPokemon(id) {
-    
     try{
-        const resposta = await fetch("https://pokeapi.co/api/v2/pokemon/25")
+        const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
 
         if(!resposta.ok) throw new Error("deu merda aqui");
-
-        const dados = await resposta.json()
-        console.log(dados);
-        modificarHtml(dados)
-         
+        return await resposta.json();
     }
     catch(error){
         console.log(error)
     }
-}
-
-function modificarHtml(dados){
-
-    const pokemonImage = document.getElementById("pokemon");
-
-    const { name: nome,
-            abilities: habilidades,
-            sprites: { other: { "official-artwork": { front_default: imagem } } },
-            types : tipo
-    } = dados
-
-    console.log(nome)
-    console.log(habilidades)
-    console.log(imagem)
-    console.log(tipo)
-
-    pokemonImage.src = imagem
 }
