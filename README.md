c857315507bf5876d28b8ca006e1f3ea8857684f
GUGOFO <gugofogomes@gmail.com>
2026-02-16 22:47:59 -0300

    Comnterio Final

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # QuemEhEssePokemon
 Um jogo de acertar qual é o pokemon, com seus tipos e habilidade!!!!
+
+## Inspiração
+![amkkds](/imgs/Inspiração.png)
+
+## Meu jogo
+![amkkds](/imgs/telaInicialJogo.png)
+
+![amkkds](/imgs/pokemonDesaparecido.png)
+
+![amkkds](/imgs/pokemonAparecido.png)
--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+/*   <3 FEITO SEM CHAT, GEMINI, IA OU QUALQUER COISA ASSIM <3
+
+  Caso vc estaja aq para ver se o chat fez isso n se preocupe,
+  esse repositorio foi feito sem a ajuda de IAs, ele serve para me ajudar
+  a virar um programador melhor e não para "preencher Github"
+  Agora fique com minhas considerações finais sobre esse projeto (boas e ruins skaska)
+
+  1 - Gostei do meu uso da API, entendi um pouco mais como se usa elas, mesmo estando longe
+  de ficar "bom" skaksakskak
+  2 - Acho que folguei um pouco no dsine de novo, sera q valeria a pena eu fazer um mini
+  curso de dsine? Possivelmente, acho q seria bem legal
+  3 - Amei fazer esse aqui, sei que é bem simples, porem ele mostra o quão facil é usar
+  uma API e como elas são uteis, quero fazer mais desses ai, msm q possivelmente eu 
+  prive esse repositorio futuramente por serem muitos basicos
+  4 - So falo uma coisa:
+      btnNomes.forEach(btn => btn.classList[0] == "botoesAcertou" ? btn.classList.replace("botoesAcertou", "botoes") : btn.classList.replace("botoesErrou", "botoes"));
+      btnHabilidades.forEach(btn => btn.classList[0] == "botoesAcertou" ? btn.classList.replace("botoesAcertou", "botoes") : btn.classList.replace("botoesErrou", "botoes"));
+      btnTipos.forEach(btn => btn.classList[0] == "botoesAcertou" ? btn.classList.replace("botoesAcertou", "botoes") : btn.classList.replace("botoesErrou", "botoes"));
+  5 - Meu deus como eu pequei no numero 4 kaskskakskaskak
+
+*/
+
 const reiniciar = document.getElementById("reiniciar");
 const btnNomes = document.querySelectorAll(".nomes");
 const btnHabilidades = document.querySelectorAll(".habilidades");
